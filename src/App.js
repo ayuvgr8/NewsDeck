fd66b25fc0be5eb2f0f5d671df108384f14d21a6
ayuvgr8 <ayuvgr8@gmail.com>
2021-09-28 19:28:35 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c = 'jhon';
+  render() {
+    return (
+      <div>
+        hello My first Class Based Component {this.c}
+
+      </div>
+    )
+  }
+}
